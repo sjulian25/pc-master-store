8ac20c75b0d13238f9457fdb4b7cb2caae36d733
1075277535 <santana9416@gmail,com>
2025-04-08 13:20:21 -0500

    actualizacion de requirements

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
-
+from routes.catalog import catalog_bp
 
 app = Flask(__name__)
+app(CORS)
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
