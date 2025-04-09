5cbda57c7101b81b148006007b16fc56cd11c325
1075277535 <santana9416@gmail,com>
2025-04-08 20:48:18 -0500

    añadi el decorador delete y su funcion elimina segun el id

--- backend/models/category.py
+++ backend/models/category.py
@@ -17,4 +17,16 @@ def get_category():
         return jsonify(categories)
     except Exception as e:
         return jsonify({'error': str(e)})
-    
+
+@category_bp.route("/delete/products/<int:id_category>", methods=['DELETE'])
+def delete_category(id_category):
+    try:
+        con = MySQLdb.connect(**cnn,cursorclass=MySQLdb.cursors.DictCursor)
+        cursor = con.cursor()
+        delete = ('DELETE FROM category WHERE id_category = %s')
+        cursor.execute(delete,(id_category,))
+        con.commit()
+        con.close()
+        return jsonify({'Mensaje':f'Categoria con ID {id_category} eliminada'})
+    except Exception as e:
+        return jsonify({'Error': str(e)})
