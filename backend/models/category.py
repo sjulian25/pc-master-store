f06fb6241e13eff47be5d585ecceabcc91e3d182
1075277535 <santana9416@gmail,com>
2025-04-08 22:58:28 -0500

    añadi el decorador update y su funcion actualizar los datos de categoria

--- backend/models/category.py
+++ backend/models/category.py
@@ -32,3 +32,21 @@ def delete_category(id_category):
         return jsonify({'Mensaje':f'Categoria con ID {id_category} eliminada'})
     except Exception as e:
         return jsonify({'Error': str(e)})
+
+@category_bp.route("/update/products/<int:id_category>", methods=['PUT'])
+def update_category(id_category):
+    data = request.json
+    name = data.get('name')
+    description = data.get('description')
+    try:
+        con = MySQLdb.connect(**cnn,cursorclass=MySQLdb.cursors.DictCursor)
+        cursor = con.cursor()
+        update = 'UPDATE category SET name = %s,description = %s WHERE id_category = %s'
+        cursor.execute(update,(name,description,id_category))
+        con.commit()
+        con.close()
+        if cursor.rowcount == 0:
+            return jsonify({'Mensaje': 'ID no encontrado'})
+        return jsonify({'Mensaje': f'Categoria con ID {id_category} actualizada'})
+    except Exception as e:
+        return jsonify({'Error': str(e)})
