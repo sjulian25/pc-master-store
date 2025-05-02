b351fae9afd24f41a69437dd8363cd81270e7c17
Santiago-202 <santiagosantanacorrea@gmail.com>
2025-05-02 08:38:19 -0500

    Feat/admin page (#15)
    * user authentication
    
    * todo el admin page
    
    ---------
    
    Co-authored-by: 1075277535 <santana9416@gmail,com>

---
+++ frontend/src/pages/AdminCategories.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+    <AdminCategoryTable />
+    </div>
+</template>
+
+<script setup>
+import AdminCategoryTable from '@/components/admin/AdminCategoryTable.vue'
+</script>
---
+++ frontend/src/pages/AdminDashboard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="admin-layout">
+    <AdminSidebar />
+    <div class="admin-content">
+        <router-view />
+    </div>
+    </div>
+</template>
+<script setup>
+import AdminSidebar from '@/components/admin/AdminSidebar.vue'
+</script>
+<style scoped>
+.admin-layout {
+    display: flex;
+    min-height: 100vh;
+    background: #f5f6fa;
+}
+.admin-content {
+    flex: 1;
+    padding: 2rem;
+}
+</style>
